a8cb07126b62d834299d5351e75a27f646b66ae2
Arya24-ok <aryagta456@gmail.com>
2025-05-03 21:06:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('voteForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const selected = document.querySelector('input[name=kandidat]checked');
+  
+  if (!selected) {
+    document.getElementById('message').textContent = 'Silakan pilih salah satu kandidat!';
+    return;
+  }
+
+  const kandidat = selected.value;
+  const waktu = new Date().toISOString();
+  const data = {
+    kandidat kandidat,
+    waktu waktu
+  };
+
+   Simpan ke localStorage untuk saat ini
+  let hasil = JSON.parse(localStorage.getItem('hasilPemilihan')  '[]');
+  hasil.push(data);
+  localStorage.setItem('hasilPemilihan', JSON.stringify(hasil));
+
+  document.getElementById('message').textContent = `Terima kasih, Anda memilih ${kandidat}.`;
+  document.getElementById('voteForm').reset();
+});
